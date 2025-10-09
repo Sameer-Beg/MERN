bf1d32c725b6f43a81e1f597ab13f3566099ae0f
SameerBeg <sameer423j@gmail.com>
2025-10-09 14:40:35 +0530

    addfiles

--- DAY2/Routing.js
+++ DAY2/Routing.js
@@ -1,17 +1,41 @@
 let http = require("http");
-let server = http.createServer((req , res )=>{
 
-    if(req.url=="/sameer"){
+let server = http.createServer((req, res) => {
+
+    if (req.url == "/sameer") {
         res.end("Hello Sameer Beg");
     }
-    if(req.url == "/tanveer"){
+    else if (req.url == "/tanveer") {
         res.end("Hello Tanveer");
     }
-
-    if(req.url == "/"){
-    res.end("Hello from the server");
+    else if (req.url == "/") {
+        res.end("Hello from the server");
     }
-})
-server.listen(4000,()=>{
+    else if (req.url == "/obj") {
+        // Create an object
+        let obj = {
+            status: 1,
+            data: [
+                {
+                    title: "iPhone",
+                    price: "34000"
+                },
+                {
+                    name: "Sameer",
+                    age: "21"
+                }
+            ]
+        };
+
+        // Send JSON response
+       
+        res.end(JSON.stringify(obj));
+    }
+    else {
+        res.end("404 Page Not Found");
+    }
+});
+
+server.listen(4000, () => {
     console.log("Server is running on port 4000...");
-})
+});
